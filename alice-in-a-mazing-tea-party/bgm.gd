56a47bc44afd596308adb7e08cb42f159c636977
Norika08 <nkatsuya@sva.edu>
2024-12-16 07:39:15 -0500

    bgm and maybe sound effect

--- alice-in-a-mazing-tea-party/bgm.gd
+++ alice-in-a-mazing-tea-party/bgm.gd
@@ -1,5 +1,4 @@
-extends Node3D
-@onready var potion_sound =$Potion_Sound # AudioStreamPlayerノードを参照
+extends AudioStreamPlayer
 
 
 # Called when the node enters the scene tree for the first time.
@@ -10,10 +9,5 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
-
-
-func _on_area_3d_body_entered(body: Node3D) -> void:
-	if body.is_in_group("Player"):
-		potion_sound.play()
-		body.pick()
-		queue_free()
+	if $AudioStreamPlayer.playing == false:
+		$AudioStreamPlayer.play()
