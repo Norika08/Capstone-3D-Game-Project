2c4552a1090fccd8004f4ab03813dcd0f2429c26
Norika08 <nkatsuya@sva.edu>
2024-12-16 05:31:32 -0500

    Game Clear Label

--- alice-in-a-mazing-tea-party/Scripts/test2.gd
+++ alice-in-a-mazing-tea-party/Scripts/test2.gd
@@ -61,7 +61,8 @@ extends CharacterBody3D
 @onready var camera_3d = $Neck/Camera3D
 @onready var neck = $Neck
 
-
+#@onready var game_clear_label = $"../GameClearScreen/CanvasLayer/Label"  # Labelノードを参照
+@onready var game_clear_label = $"../GameClearScreen/CanvasLayer/Label"
 
 @onready var game_clear_camera = $"/root/Node3D/Game_Clear_Camera"  # Game_Clear_Camera ノードを参照
 
@@ -70,6 +71,7 @@ extends CharacterBody3D
 var start_position: Vector3 = Vector3(-4.095, 5.407, 0)
 var Potion = 0
 
+
 const SPEED = 0.5
 const JUMP_VELOCITY = 2.5
 
@@ -77,8 +79,28 @@ const JUMP_VELOCITY = 2.5
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 
 
-
-
+#func _ready():
+	#if game_clear_label:
+		#game_clear_label.visible = false
+	#else:
+		#print("Error: Game Clear Label not found")
+#func _ready():
+	#if game_clear_label:
+		#game_clear_label.visible = false
+		#print("Game Clear label initialized.")
+	#else:
+		#print("Error: Game Clear Label not found")
+#func _ready():
+	#if game_clear_label:
+		#game_clear_label.visible = false  # ゲーム開始時は非表示
+# ゲーム開始時にラベルを非表示にする
+func _ready():
+	# ラベルの参照が正しいかを確認する
+	if game_clear_label:
+		game_clear_label.visible = false  # ゲーム開始時は非表示にする
+		print("Game Clear label initialized and set to invisible.")
+	else:
+		print("Error: Game Clear Label not found")
 
 func _physics_process(delta):
 	# Add gravity
@@ -143,6 +165,7 @@ func pick():
 func check_game_clear():
 	if Potion >= 7:
 		print("Game Clear!")
+		show_game_clear_label()  # ゲームクリア時にラベル表示を呼び出す
 		# Switch to the Game Clear View
 		#switch_to_game_clear_view()
 		switch_to_game_clear_camera()
@@ -181,3 +204,45 @@ func reset_position() -> void:
 	print("Resetting position to start!")
 	global_transform.origin = start_position  # 初期位置に戻す
 	velocity = Vector3.ZERO  # 動きをリセット
+
+
+#func show_game_clear_text():
+	## "Game Clear!!" を表示
+	#if game_clear_label:
+		##game_clear_label.text = "Game Clear!!"
+		#game_clear_label.visible = true
+#
+		 ##3秒後にテキストを非表示にする
+		##await get_tree().create_timer(3.0).timeout
+		##game_clear_label.visible = false
+#func show_game_clear_label():
+	#if game_clear_label:
+		#game_clear_label.text = "How wonderful it is to be whole again! 
+#Now, let us begin our curious tea party!"
+		#game_clear_label.visible = true
+		#await get_tree().create_timer(3.0).timeout
+		#game_clear_label.visible = false
+	#else:
+		#print("Error: Game Clear Label not found")
+#func show_game_clear_label():
+	#if game_clear_label:
+		#game_clear_label.text = "Game Clear!!"
+		#game_clear_label.visible = true
+		#print("Game Clear label is now visible!")
+		#await get_tree().create_timer(3.0).timeout
+		#game_clear_label.visible = false
+	#else:
+		#print("Error: Game Clear Label not found")
+#func show_game_clear_label():
+	#if game_clear_label:
+		#game_clear_label.visible = true  # ラベルを表示
+		#print("Game Clear label is now visible!")
+	#else:
+		#print("Error: Game Clear Label not found")
+# ゲームクリアラベル表示
+func show_game_clear_label():
+	if game_clear_label:
+		game_clear_label.visible = true  # ラベルを表示
+		print("Game Clear label is now visible!")
+	else:
+		print("Error: Game Clear Label not found")
